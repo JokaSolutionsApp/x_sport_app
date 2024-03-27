6429587d36ba581cb887673dd13116b30e8bad75
Yahya <jokasolutions.app@gmail.com>
2024-03-27 14:05:03 +0300

    UI adjustments

---
+++ lib/app/features/home/presentation/components/memberships_banner.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:x_sport/core/constance/app_constance.dart';
+
+class MembershipsBanner extends StatelessWidget {
+  const MembershipsBanner({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Card(
+        margin: const EdgeInsets.all(25),
+        color: XColors.primary,
+        surfaceTintColor: XColors.primary,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'العضويات',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      color: XColors.white,
+                    ),
+                  ),
+                  Text(
+                    'تابع اشتراكك في الاكاديميات التي سجلت بها',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
+                      color: XColors.white,
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                height: 44,
+                width: 44,
+                decoration: const BoxDecoration(
+                  color: XColors.white,
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(
+                  Icons.arrow_forward_ios,
+                  color: XColors.primary,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/app/features/home/presentation/pages/academies_page.dart
+++ lib/app/features/home/presentation/pages/academies_page.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:x_sport/app/features/home/presentation/components/memberships_banner.dart';
+
 import '../../../academy/domain/enitites/params/acedemy_params.dart';
 import '../../../academy/presentation/bloc/academy_bloc.dart';
 import '../components/academies_components/academies_component.dart';
-import '../components/academies_components/membership_component.dart';
+
 import '../components/academies_components/suggested_academies_component.dart';
 import '../../../profile/presentation/components/profile_screen_components/profile_main_components/profile_appbar_component.dart';
 import '../components/academies_components/academies_categories_component.dart';
-import '../../../../controllers/fileds_bloc.dart';
-import '../../../../widgets/text_fields/search_textfield.dart';
 
 class AcademiesPage extends StatefulWidget {
   const AcademiesPage({super.key});
@@ -37,23 +38,7 @@ class _AcademiesPageState extends State<AcademiesPage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Padding(
-              padding:
-                  EdgeInsets.symmetric(horizontal: 0.04.sw, vertical: 10.w),
-              child: Column(
-                children: [
-                  SearchField(
-                    hintText: '...ابحث عن لاعب',
-                    controller: search,
-                    keyboardType: TextInputType.emailAddress,
-                    textStream: academiesSearchStream.name,
-                    onChanged: academiesSearchStream.changeName,
-                  ),
-                  SizedBox(height: 10.w),
-                  const MemberShipComponent(),
-                ],
-              ),
-            ),
+            const MembershipsBanner(),
             AcademeiesCategoriesComponent(),
             SizedBox(height: 26.h),
             const SuggestedAcademiesComponent(),
