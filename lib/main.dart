9a619cb23307f577f767ab6834dd15087f3378f0
Yahya <jokasolutions.app@gmail.com>
2024-04-02 00:41:10 +0300

    minor

--- lib/main.dart
+++ lib/main.dart
@@ -89,7 +89,7 @@ class MyApp extends StatelessWidget {
               ),
               useMaterial3: true,
             ),
-            home: const ProfileRankingPage(),
+            home: const RoutePage(),
           ),
         );
       },
