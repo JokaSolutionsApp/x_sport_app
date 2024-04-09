954fca4fc73f20006b0b7fff2c60c50f825a869a
ammar_kseibati <ammarkseibati@technozleader.com>
2024-04-09 14:20:13 +0300

    change password

--- lib/main.dart
+++ lib/main.dart
@@ -83,7 +83,6 @@ class MyApp extends StatelessWidget {
               useMaterial3: true,
             ),
             home: const RoutePage(),
-            home: const RoutePage(),
           ),
         );
       },
